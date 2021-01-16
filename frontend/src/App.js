4faf09a2cc3ea204e1bf5a96360b7ecdc4a99633
nikashan02 <nikashan02@gmail.com>
2021-01-15 23:42:38 -0500

    Added Google Analytics

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,102 +1,28 @@
-import React, { Component } from 'react';
-import './App.css'
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import EzShort from './EzShort';
+import ReactGA from 'react-ga';
+import { BrowserRouter as Router, Route, withRouter } from 'react-router-dom';
 import Redirect from './Redirect';
-import axios from 'axios';
-import validator from 'validator';
-import Swal from 'sweetalert2';
-import {CopyToClipboard} from 'react-copy-to-clipboard';
+require('dotenv').config();
 
-class App extends Component {
+ReactGA.initialize(process.env.GA_KEY); 
 
-  constructor() {
-    super();
-    this.state = {
-      hidden: "none",
-      fullUrl: "",
-      shortUrl: ""
-    };
-  }
+function App() {
 
-  shorten = (event) => {
-    event.preventDefault();
-    let valid = validator.isURL(this.state.fullUrl);
-    const Toast = Swal.mixin({
-      toast: true,
-      position: 'top-end',
-      showConfirmButton: false,
-      timer: 3000,
-      timerProgressBar: true,
-      didOpen: (toast) => {
-        toast.addEventListener('mouseenter', Swal.stopTimer)
-        toast.addEventListener('mouseleave', Swal.resumeTimer)
-      }
-    })
-    if(valid) {
-      axios.post('api/endpoints/shortUrls', {'first': this.state.fullUrl})
-      .then((res) => {
-        this.setState ({
-          shortUrl: `${"/"+res.data.short}`
-        });
-        this.setState({hidden: "flex"});
-        Toast.fire({
-          icon: 'success',
-          title: 'Shortened URL created'
-        })
-      }, (error) => {
-        console.log(error);
-      });
-    } else {      
-      Toast.fire({
-        icon: 'error',
-        title: 'Please enter a valid URL'
-      })
-    }
-  }
+  useEffect( () => {
+    ReactGA.pageview(window.location.pathname + window.location.search); 
+  });
 
-  setFullUrl = (event) => {
-    this.setState({fullUrl: event.target.value})
-  }
-
-  render() {
-    return (
-      <Router>
-        <div className="container">
-          <Route path="/" exact render={() => (
-            <div>
-              
-              <header style={{display: "flex", justifyContent: "center", alignItems: "flex-end", height: "40vh"}}>
-                <h1 class="head">ezShort</h1>
-              </header>
-
-              <div style={{display: "flex", justifyContent: "center", alignItems: "flex-end", height: "2vh"}}>
-                <h2><a href="https://github.com/nikashan02/ezshort">Github</a></h2>
-              </div>
-              
-              <main>
-                <form className="form-inline">
-                  <fieldset style={{display: "flex", justifyContent: "center", alignItems: "flex-start"}}>
-                    <input className="urlInput" placeholder="https://www.google.ca" onChange={this.setFullUrl} />
-                    <input className="shortenBtn" type="button" value="Shorten" onClick={this.shorten} />
-                  </fieldset>
-                </form>
-                <div style={{display: `${this.state.hidden}`, justifyContent: "center", alignItems: "flex-start"}}>
-                  <div className="short">
-                    <a href={this.state.shortUrl}><span className="link">{"ezshort.herokuapp.com"+this.state.shortUrl}</span></a>
-                    <CopyToClipboard text={"ezshort.herokuapp.com"+this.state.shortUrl} onCopy={() => this.setState({copied: true})}>
-                      <button className="copy" title="Copy to Clipboard"/>
-                    </CopyToClipboard>
-                  </div>
-                </div>
-              </main>
-
-            </div>
-          )} />
-          <Route path="/:hash" component={Redirect} exact />
-        </div>
-      </Router>
-    );
-  }
+  return (
+    <Router>
+      <div className="container">
+        <Route path="/" exact render={() => (
+          <EzShort/>
+        )} />
+        <Route path="/:hash" component={Redirect} exact />
+      </div>
+    </Router>
+  );
 }
 
-export default App;
+export default withRouter(App); 
---
+++ frontend/src/EzShort.js
@@ -0,0 +1,94 @@
+import React, { Component} from 'react';
+import './App.css'
+import axios from 'axios';
+import validator from 'validator';
+import Swal from 'sweetalert2';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
+
+class EzShort extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      hidden: "none",
+      fullUrl: "",
+      shortUrl: ""
+    };
+  }
+
+  shorten = (event) => {
+    event.preventDefault();
+    let valid = validator.isURL(this.state.fullUrl);
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+      }
+    })
+    if(valid) {
+      axios.post('api/endpoints/shortUrls', {'first': this.state.fullUrl})
+      .then((res) => {
+        this.setState ({
+          shortUrl: `${"/"+res.data.short}`
+        });
+        this.setState({hidden: "flex"});
+        Toast.fire({
+          icon: 'success',
+          title: 'Shortened URL created'
+        })
+      }, (error) => {
+        console.log(error);
+      });
+    } else {      
+      Toast.fire({
+        icon: 'error',
+        title: 'Please enter a valid URL'
+      })
+    }
+  }
+
+  setFullUrl = (event) => {
+    this.setState({fullUrl: event.target.value})
+  }
+
+  render() {
+
+    return (
+      <div>
+        
+        <header style={{display: "flex", justifyContent: "center", alignItems: "flex-end", height: "40vh"}}>
+          <h1 class="head">ezShort</h1>
+        </header>
+
+        <div style={{display: "flex", justifyContent: "center", alignItems: "flex-end", height: "2vh"}}>
+          <h2><a href="https://github.com/nikashan02/ezshort">Github</a></h2>
+        </div>
+        
+        <main>
+          <form className="form-inline">
+            <fieldset style={{display: "flex", justifyContent: "center", alignItems: "flex-start"}}>
+              <input className="urlInput" placeholder="https://www.google.ca" onChange={this.setFullUrl} />
+              <input className="shortenBtn" type="button" value="Shorten" onClick={this.shorten} />
+            </fieldset>
+          </form>
+          <div style={{display: `${this.state.hidden}`, justifyContent: "center", alignItems: "flex-start"}}>
+            <div className="short">
+              <a href={this.state.shortUrl}><span className="link">{"ezshort.herokuapp.com"+this.state.shortUrl}</span></a>
+              <CopyToClipboard text={"ezshort.herokuapp.com"+this.state.shortUrl} onCopy={() => this.setState({copied: true})}>
+                <button className="copy" title="Copy to Clipboard"/>
+              </CopyToClipboard>
+            </div>
+          </div>
+        </main>
+
+      </div>
+    );
+  }
+}
+
+export default EzShort;
